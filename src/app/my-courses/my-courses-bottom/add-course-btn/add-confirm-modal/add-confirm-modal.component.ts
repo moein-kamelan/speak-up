235858f81f74cf7efd591bebd5ed5f27083b9b70
Moein Kamelan <M.kamelan@pishgam.local>
2025-04-08 16:07:07 +0330

    finish crud

--- src/app/my-courses/my-courses-bottom/add-course-btn/add-confirm-modal/add-confirm-modal.component.ts
+++ src/app/my-courses/my-courses-bottom/add-course-btn/add-confirm-modal/add-confirm-modal.component.ts
@@ -1,12 +1,14 @@
-import { Component, InjectionToken } from '@angular/core';
+import { Component, InjectionToken, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import { MaterialModule } from '../../../../material/material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { DialogRef } from '@angular/cdk/dialog';
 import { CoursesService } from '../../../../services/courses.service';
 import { Course } from '../../../../models/courses';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule, NgFor } from '@angular/common';
+import { CourseStatus } from '../../../../course-status';
 @Component({
   selector: 'app-add-confirm-modal',
   imports: [MatInputModule , 
@@ -14,23 +16,45 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
     MaterialModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule ],
+    FormsModule ,
+    CommonModule
+  ],
   templateUrl: './add-confirm-modal.component.html',
   styleUrl: './add-confirm-modal.component.css'
 })
-export class AddConfirmModalComponent {
+export class AddConfirmModalComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<AddConfirmModalComponent>,
-    private coursesService: CoursesService,
+    private coursesService: CoursesService
     
-  ) {}
+  ) {
+    console.log("coursesService" , coursesService);
 
+  }
+  courseStatus = CourseStatus
+  statusOptions: string[] = [];
+
+  ngOnInit(): void {
+    this.statusOptions = Object.values(this.courseStatus)
+    
+    
+  }
   onCancel() {
     this.dialogRef.close(false)
 
   }
-onConfirm() {
+onConfirm(formValid:any) {
   this.dialogRef.close(true)
+
+}
+
+onSubmit(form:NgForm) {
+console.log(form.value);
+const newCourse = {  img : "/images/hero-img.png" , ...form.value }
+    console.log("newCourse" ,newCourse);
+
+    this.coursesService.createCourse(newCourse)
+
 }
   
 
