55973ddae9bf45e18491860bfa18ac9d44bd56f6
moein801 <moeinkamelan@gmail.com>
2025-04-09 23:28:54 +0430

    final commit in home on 2025/4/9

--- src/app/my-courses/my-courses-bottom/add-course-btn/add-course-btn.component.ts
+++ src/app/my-courses/my-courses-bottom/add-course-btn/add-course-btn.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { AddConfirmModalComponent } from './add-confirm-modal/add-confirm-modal.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MaterialModule } from '../../../material/material.module';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { ModalComponent } from '../../../shared/modal/modal.component';
+import { CourseStatus } from '../../../course-status';
 
 @Component({
   selector: 'app-add-course-btn',
@@ -22,8 +24,25 @@ export class AddCourseBtnComponent {
   constructor(private dialog: MatDialog) {} 
   onAddClick() {
 
-    const dialogRef = this.dialog.open(AddConfirmModalComponent , {
-      disableClose:true
+    const dialogRef = this.dialog.open(ModalComponent , {
+      disableClose:true,
+      data : {
+        title : "اضافه کردن دوره",
+        fields : [
+          {name : "title" , label : "عنوان" , type : "string"},
+          {name : "level" , label : "سطح" , type : "string"},
+          {name : "term" , label : "ترم" , type : "string"},
+          {name : "startTime" , label : "زمان شروع" , type : "string"},
+          {name : "members" , label : "تعداد عضو" , type : "number"},
+          {name : "status" , label : "وضعیت" , type : "select" , options : [CourseStatus.InProgress , CourseStatus.WaitingForCapacity , CourseStatus.WaitingToStart , CourseStatus.Finished]},
+          {name : "date" , label : "تاریخ" , type : "string"},
+          {name : "hour" , label : "ساعت" , type : "string"},
+        ]
+        ,
+        submitButtonText : "اضافه"
+        ,
+        submitButtonIcon : "add"
+      }
     })
 
     dialogRef.afterClosed().subscribe((result: boolean) => {
--- src/app/my-courses/my-courses-bottom/my-courses-card/my-courses-card-desktop/my-courses-card-desktop.component.ts
+++ src/app/my-courses/my-courses-bottom/my-courses-card/my-courses-card-desktop/my-courses-card-desktop.component.ts
@@ -5,66 +5,77 @@ import { MaterialModule } from '../../../../material/material.module';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DeleteConfirmDialogComponent } from './delete-confirm-dialog/delete-confirm-dialog.component';
 import { EditConfirmModalComponent } from './edit-confirm-modal/edit-confirm-modal.component';
-import { AddConfirmModalComponent } from '../../add-course-btn/add-confirm-modal/add-confirm-modal.component';
 import { CourseStatusDirective } from '../../../../directives/course-status.directive';
+import { ModalComponent } from '../../../../shared/modal/modal.component';
+import { data } from 'jquery';
+import { CourseStatus } from '../../../../course-status';
 @Component({
   selector: 'app-my-courses-card-desktop',
-  imports: [RouterModule , RouterLink  , MaterialModule , MatDialogModule , CourseStatusDirective],
+  imports: [
+    RouterModule,
+    RouterLink,
+    MaterialModule,
+    MatDialogModule,
+    CourseStatusDirective,
+  ],
   templateUrl: './my-courses-card-desktop.component.html',
   styleUrl: './my-courses-card-desktop.component.css',
 })
 export class MyCoursesCardDesktopComponent implements OnInit {
-@Input() course!:Course
+  @Input() course!: Course;
 
-constructor(private dialog: MatDialog) {
-  
-}
+  constructor(private dialog: MatDialog) {}
 
-onDeleteClick( id:number ) {
-  
-  const dialogRef = this.dialog.open(DeleteConfirmDialogComponent , {data : {id : id} })
+  onDeleteClick(id: number) {
+    const dialogRef = this.dialog.open(DeleteConfirmDialogComponent, {
+      data: { id },
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    if (result === true) {
-      console.log('حذف تأیید شد');
-    } else {
-      console.log('حذف لغو شد');
-    }
-  });
-}
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === true) {
+        console.log('حذف تأیید شد');
+      } else {
+        console.log('حذف لغو شد');
+      }
+    });
+  }
 
-onEditClick(course: Course) {
-  const dialogRef = this.dialog.open(EditConfirmModalComponent , {data : {course} })
+  onEditClick(course: Course) {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      data: {
+        course,
+        data: {
+          title : "ویرایش دوره",
+          fields: [
+            { name: 'title', label: 'عنوان', type: 'string' },
+            { name: 'level', label: 'سطح', type: 'string' },
+            { name: 'term', label: 'ترم', type: 'string' },
+            { name: 'startTime', label: 'زمان شروع', type: 'string' },
+            { name: 'members', label: 'تعداد عضو', type: 'number' },
+            { name: 'status', label: 'وضعیت', type: 'select' , options : [CourseStatus.InProgress , CourseStatus.WaitingForCapacity , CourseStatus.WaitingToStart , CourseStatus.Finished] },
+            { name: 'date', label: 'تاریخ', type: 'string' },
+            { name: 'hour', label: 'ساعت', type: 'string' },
+          ],
+          submitButtonText: 'ویرایش',
+          submitButtonIcon: 'edit',
+          defaultValuess : {...course}
+        },
+      },
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    if (result === true) {
-    
-      console.log('ویرایش تأیید شد');
-    } else {
-      console.log('ویرایش لغو شد');
-    }
-  });
-  
-  
-  
-}
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === true) {
+        console.log('ویرایش تأیید شد');
+      } else {
+        console.log('ویرایش لغو شد');
+      }
+    });
+  }
 
+  ngOnInit(): void {}
 
-
-ngOnInit(): void {
-  
-}
-
-
-
-
-togglePopup(event:Event , popup:any) {
-  event.preventDefault()
-  event.stopPropagation()
-
-
-  
-  
-
-}
+  togglePopup(event: Event, popup: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
 }
